1ce75f68405e3495bf2e4229ed2ba190cbc845e6
Aomx <julian.cordes@gmail.com>
2014-12-04 11:37:13 +0000

    added progress bar

--- SHA-1/main.c
+++ SHA-1/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <math.h>
 
 #include "sha1.h"
 #include "../Utils/utils.h"
 #include "../Utils/bruteforce.h"
 
 #define PW_LEN 5
+
+unsigned long counter = 0;
+unsigned long needToCheck = 0;
+char *currentPassword = "a";
+
 
 void update_sha1 (void *buffer, unsigned int bytesRead, void *ctx) {
 	SHA1_CTX *sha_context = (SHA1_CTX*) ctx;
@@ -34,16 +42,13 @@ void get_sha1_from_string(SHA1_CTX *ctx, char *string, uchar *hash) {
 
 int checkPasswordSHA1 (void *ctx, char *password, uchar *toBreakHash) {
 	static uchar passwordHash[SHA1_SIZE];
-	static int counter = 0;
-	
+
+	currentPassword = password;	
+
 	counter++;
 
 	
 	get_sha1_from_string( ctx, password, passwordHash);
-
-	if ((counter % 100000) == 0) {			
-		printf("%s\n", password);
-	}
 	
 	if (sha1_equal(toBreakHash, passwordHash)) {
 		printf("broken the hash! password was %s\n", password);
@@ -52,16 +57,48 @@ int checkPasswordSHA1 (void *ctx, char *password, uchar *toBreakHash) {
 	return 0;
 }
 
+void handleAlarm(int signal) {
+	static unsigned long counterBefore;
+	static unsigned long counterNow;
+
+	counterBefore = counterNow;
+	counterNow = counter;
+	
+	double curProcess = ((double)counterNow / (double) needToCheck);
+	
+	alarm(1);
+	
+	printf("%s %lu pws/s total:  %.2f%%\n", currentPassword, counterNow - counterBefore, curProcess*100);
+}
+
+unsigned long pow_ul (unsigned long base, unsigned long exp) {
+	unsigned long t = 1;
+	unsigned long i = 0;
+	
+	for (i = 0; i < exp; i++) {
+		t *= base;
+	}
+		
+	return t;
+}
+
 int main (int argc, char **argv) {
 	//int i, j, k, l;
-	char passphrase[]={"aa1aa"};
+	char passphrase[]={"99999"};
 	char alphabet[] = {"abcdefghiklmnopqrstuvwxyzABCDEFGHIKLMOPQRSTUVXYZ123456789"};
+	
+	needToCheck = pow_ul(strlen(alphabet), PW_LEN);
 	
 	uchar toBreakHash[SHA1_SIZE];
 	
 	SHA1_CTX sha_context;
 
 	get_sha1_from_string( &sha_context, passphrase, toBreakHash);
+	
+	signal(SIGALRM, handleAlarm );
+
+
+	alarm(1);
 	
 	bruteforcePasswordAll(&sha_context, toBreakHash, checkPasswordSHA1, alphabet, PW_LEN);
 	
