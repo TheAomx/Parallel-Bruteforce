d159892c28f052c0f2cf7a50ab1fe9d69348370a
WorstConn <nfrenking@gmail.com>
2015-01-13 21:12:02 +0100

    .Hashing function pointer interface definitions and documentation

---
+++ ParallelBruteforce/pb_common.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+
+#include "pb_common.h"
+
+
+char* nextGenerationPassword(char* currentPassword, char* alphabet, unsigned int count) {
+    return NULL;
+}
+
+unsigned long passwordDifference(char* password1, char* password2, char* alphabet) {
+    return 0;
+}
+
+
+AlphaValueMapping* assignValues(char* alphabet) {
+   return NULL; 
+}
+
+
+unsigned long calcNumPasswords(unsigned long alphabetLength, unsigned long maxPasswordLength) {
+    return 0;
+}
+
+
