a31c1a4226e15959b245d8decb022fef804a567e
Aomx <julian.cordes@gmail.com>
2015-02-10 20:55:49 +0000

    moved utils to singlecore project

--- Utils/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-typedef void (*FileReader) (void *buffer, unsigned int bytesRead, void *ctx);
-void readFile(char *filename, FileReader callback, void *ctx);
-unsigned long pow_ul (unsigned long base, unsigned long exp);
-unsigned long calcNumPasswords(unsigned long alphabetLength, unsigned long maxPasswordLength);
-#endif
